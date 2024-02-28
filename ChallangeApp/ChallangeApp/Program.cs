e4c9ca9aa74723feb291a87ab59ab9bfbb31c10a
zd3nq <zdenek.jg@wp.pl>
2024-02-28 19:54:47 +0100

    zadanie dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int age = 30;
+string name = "Ewa";
+string sex = "kobieta";
+//
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+
+else if (sex != "kobieta" && name != "Ewa" && age != 30)
+{
+    Console.WriteLine("   ");
+}
